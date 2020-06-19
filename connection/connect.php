c3afaf549937dee49525e49da2da67d2903c3ce3
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-19 09:02:58 -1200

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,6 +1,5 @@
 <?php
 	//Connect to database    
-    $url = (getenv("mysql://j2jujn9suxbx1su1:irzbk1tormac7dh3@d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ncgwmoi6ngjlg75t"));
     $servername='d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     $username='j2jujn9suxbx1su1';
     $password='irzbk1tormac7dh3';
