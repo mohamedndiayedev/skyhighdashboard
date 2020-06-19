0533ff2d4105aa616d7de20282e4712033517087
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-18 15:52:42 -1200

    db updated for heroku

--- connection/connect.php
+++ connection/connect.php
@@ -3,7 +3,7 @@
     $servername='d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     $username='j2jujn9suxbx1su1';
     $password='irzbk1tormac7dh3';
-    $dbname = "ncgwmoi6ngjlg75t";
+    $dbname = 'ncgwmoi6ngjlg75t';
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
     if(!$conn){
 		die('Could not Connect My Sql:' .mysql_error());
