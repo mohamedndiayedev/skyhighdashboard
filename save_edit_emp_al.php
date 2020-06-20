878b3d481f2781a8fa31919296e37693cca94b65
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-19 23:30:16 -1200

    Update save_edit_emp_al.php

--- save_edit_emp_al.php
+++ save_edit_emp_al.php
@@ -14,7 +14,7 @@
 				showConfirmButton: false,
 			},
 			window.load = function(){
-				window.location='http://localhost/skyhighdashboard/view_Employee.php';
+				window.location='view_Employee.php';
 			});
 		}
 	</script>
