a0492a589c4b7f471eaf779ff5cb2bd5b47cfaf1
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-19 20:28:32 -1200

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,9 +1,9 @@
 <?php
 	//Connect to database    
-    $servername='localhost';
-    $username='root';
-    $password='';
-    $dbname = 'hrm_project';
+    $servername='d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $username='ioz2i6mmtivthmx6';
+    $password='az5hz3fmk9ludp1e';
+    $dbname = 'nyj2sbh0wlxx3dv9';
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
     if(!$conn){
 		die('Could not Connect My Sql:' .mysql_error());
