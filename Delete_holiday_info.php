d7c1b55928055e53dbc9b877cfe109fef1ee039d
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-20 03:03:03 -1200

    Update Delete_holiday_info.php

--- Delete_holiday_info.php
+++ Delete_holiday_info.php
@@ -14,7 +14,7 @@
         showConfirmButton: false,
       },
       window.load = function(){
-        window.location='http://localhost/skyhighdashboard/holiday_info.php';
+        window.location='holiday_info.php';
       });
     }
   </script>
