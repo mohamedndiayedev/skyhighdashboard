ec5e7264ab51334c79ce4d67bf3f9bc174dc3ef9
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-20 03:05:18 -1200

    Update Delete_salary_info.php

--- Delete_salary_info.php
+++ Delete_salary_info.php
@@ -14,7 +14,7 @@
         showConfirmButton: false,
       },
       window.load = function(){
-        window.location='http://localhost/skyhighdashboard/Salary_Info.php';
+        window.location='Salary_Info.php';
       });
     }
   </script>
