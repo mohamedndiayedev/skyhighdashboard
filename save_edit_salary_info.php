8394c924279fdfd4e29b1212115135a2cf0031af
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-20 02:23:37 -1200

    Update save_edit_salary_info.php

---
+++ save_edit_salary_info.php
@@ -0,0 +1,23 @@
+<?php
+    include 'includes/header.php' ;
+	include 'signin_checker.php' ;
+    include 'connection/connect.php';
+?>
+<header>
+	<script>
+		function MyModifyFn(){
+			swal({
+				title: "Salary Modified Successfully",
+				text: "",
+				type: "success",
+				timer: 2000,
+				showConfirmButton: false,
+			},
+			window.load = function(){
+				window.location='Salary_Info.php';
+			});
+		}
+	</script>
+	
+</header>
+?>
